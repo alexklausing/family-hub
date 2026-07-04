fecda8ca67179b25500dff1f211d7e18a1aa6021
Alex Klausing <alex.klausing@gmail.com>
2026-07-04 16:59:48 -0400

    feat: improve virtual keyboard layout by removing duplicate close button

--- resources/js/components/VirtualKeyboard.vue
+++ resources/js/components/VirtualKeyboard.vue
@@ -112,14 +112,14 @@ onMounted(() => {
                 "q w e r t y u i o p [ ] \\",
                 "a s d f g h j k l ; ' {enter}",
                 "{shift} z x c v b n m , . / {shift}",
-                "{close} @ {space} {close}"
+                "@ {space} .com {close}"
             ],
             shift: [
                 "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
                 "Q W E R T Y U I O P { } |",
                 "A S D F G H J K L : \" {enter}",
                 "{shift} Z X C V B N M < > ? {shift}",
-                "{close} @ {space} {close}"
+                "@ {space} .com {close}"
             ]
         },
         display: {
