4ca11165058d9d0b5e59faeb3d1cf72282fae491
Alex Klausing <alex.klausing@gmail.com>
2026-07-04 11:43:36 -0400

    Remove backend hardcoded cursor hide logic from blade template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Family Hub</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @if(app()->environment('production') || env('HIDE_CURSOR', false))
-        <style>
-            * {
-                cursor: none !important;
-            }
-        </style>
-        @endif
+
     </head>
     <body class="antialiased font-sans">
         <div id="app"></div>
