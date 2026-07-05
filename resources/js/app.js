1adc869353c2039ae3de6d6ee76f5cde7d050bdc
Alex Klausing <alex.klausing@gmail.com>
2026-07-05 09:57:10 -0400

    Add mobile-drag-drop polyfill to support HTML5 drag and drop on touchscreens

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,14 @@
 import './bootstrap'
 import { createApp } from 'vue'
+import { polyfill } from 'mobile-drag-drop'
+import { scrollBehaviourDragImageTranslateOverride } from 'mobile-drag-drop/scroll-behaviour'
+import 'mobile-drag-drop/default.css'
+
+polyfill({
+    dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride
+})
+
+window.addEventListener('touchmove', function() {}, { passive: false })
 import Dashboard from './components/Dashboard.vue'
 
 const app = createApp(Dashboard)
